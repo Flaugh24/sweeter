07a29907e31c0e6ef818473b00cdc80fc3702d0a
Ivan Gagarkin <gagarkin.iiu@gmail.com>
2019-01-07 17:22:30 +0300

    Initial commit

---
+++ src/test/java/com/gagarkin/sweeter/SweeterApplicationTests.java
@@ -0,0 +1,17 @@
+package com.gagarkin.sweeter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SweeterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
